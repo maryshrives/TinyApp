e195a82ae1a83a5e032d8a96097ded9eada5ca08
Mary Shrives <mshrives@alumni.yorku.ca>
2017-12-05 21:34:52 +0000

    putting get requests in correct order

--- express-server.js
+++ express-server.js
@@ -1,8 +1,20 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+function generateRandomString(n){
+  const charSet = "1234567890abcdefghijklmnopqrstuvwxyz";
+  let randomString = "";
+  for (var i = 0; i < n; i++){
+    let rnum = Math.floor(Math.random() * charSet.length);
+    randomString += charSet[rnum];
+  }
+  return randomString;
+};
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -19,6 +31,27 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls-index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls-new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase };
+  res.render("urls-show", templateVars);
+});
+
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
