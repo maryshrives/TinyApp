e680d16113ef118240978a4eac229527fc186915
Mary Shrives <mshrives@alumni.yorku.ca>
2017-12-06 16:24:38 +0000

    added delete function

--- express-server.js
+++ express-server.js
@@ -17,7 +17,7 @@ function generateRandomString(n){
 };
 // console.log(generateRandomString(6));
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
   "v1k6e9": "http://www.twitter.com"
@@ -57,6 +57,13 @@ app.post("/urls", (req, res) => {
   res.redirect("/urls/" + randomURL);         // Respond with 'Ok' (we will replace this)
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  var deleteID = req.params.id;
+  delete urlDatabase[deleteID];
+  console.log("URL has been deleted");
+  res.redirect('/urls');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -67,3 +74,19 @@ app.get("/u/:shortURL", (req, res) => {
   console.log(longURL);
   res.redirect(longURL);
 });
+
+
+// app.post("/urls/:shortURL/update", (req, res) => {
+//   let longUrl = req.body.longUrl;
+//   let shortURL = req.params.shortURL;
+//   urlDatabase[shortURL] = longUrl;
+//   res.redirect("/urls");
+// });
+
+
+
+
+
+
+
+
